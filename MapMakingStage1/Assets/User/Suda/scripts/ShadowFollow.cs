ccc8e3035b8522c41127bd1957fe036ade6e2dac
SudaKakuto <exce0304@gmail.com>
2019-04-08 06:26:03 +0900

    SunSpotDirectionを追加。 太陽の強い光、その判定するシリンダーを追加。 Nightオブジェクトを追加。

---
+++ MapMakingStage1/Assets/User/Suda/scripts/ShadowFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowFollow : MonoBehaviour {
+
+    //Sunの変数
+    public GameObject LookPlanet;
+
+    // initialization
+    void Start()
+    {
+
+    }
+
+    // Update
+    void Update()
+    {
+        //Sunの向き更新
+        transform.LookAt(LookPlanet.transform);
+    }
+}
