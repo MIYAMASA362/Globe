f3597dc8bc0730cf8360e87be971950372f5d477
ryouhei <ryouhei05205005@gmail.com>
2019-04-13 18:07:02 +0900

    指定したタグのオブジェクトが当たることで徐々に指定したオブジェクトが小さくなって消滅するスクリプトの追加

--- MapMakingStage1/Assets/User/Suzuki/Scripts/DecreaseObject.cs
+++ MapMakingStage1/Assets/User/Suzuki/Scripts/DecreaseObject.cs
@@ -7,13 +7,42 @@ using UnityEngine;
 public class DecreaseObject : MonoBehaviour {
 
     [Tag] public string Tag;
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log(Tag);
-	}
+    [SerializeField] float value = 0f;
+    [SerializeField] Transform ScaleDown;
+
+    float InitValue = 0f;
+    Vector3 InitScale = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+
+        InitValue = value;
+        InitScale = ScaleDown.localScale;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        this.ScaleDown.localScale = InitScale * Mathf.Min(value / InitValue, 1f);
+
+        if (value <= 0f)
+        {
+            this.gameObject.SetActive(false);
+        }
+
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("name:" + other.transform.name);
+
+        if (other.transform.tag == Tag)
+        {
+            Debug.Log("Stay!!!");
+
+            value -= 0.1f;
+        }
+    }
+
 }
