cbf5c6d44c74acab9bb72187b54b9bbf51766922
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-04-16 14:03:50 +0900

    MySceneManagerを使ったTitleの振る舞いを設定

---
+++ MapMakingStage1/Assets/User/Ikeda/Scripts/Scene/TitleScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : SceneBase {
+
+	// Use this for initialization
+    public override void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(MySceneManager.GalaxySelect);
+        }
+	}
+}
