6683c1be202c6a4cf2fd631ddc8f58b58dddfcc8
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-04-16 14:07:36 +0900

    Pauseを設定

---
+++ MapMakingStage1/Assets/User/Ikeda/Scripts/Scene/PauseScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class PauseScene : SceneBase {
+
+    private bool ReSetPlanet = false;
+    private bool ReSetGalaxy = false;
+    private bool ReternTitle = false;
+
+	// Use this for initialization
+	public override void Start ()
+    {
+        ReSetPlanet = false;
+        ReSetGalaxy = false;
+        ReternTitle = false;
+    }
+	
+	// Update is called once per frame
+	public override void Update ()
+    {
+        if (ReSetPlanet) MySceneManager.Load_Planet();
+        if (ReSetGalaxy) MySceneManager.Load_Galaxy();
+        if (ReternTitle) SceneManager.LoadScene(MySceneManager.TitleScene);
+
+	}
+
+}
