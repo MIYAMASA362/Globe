1384a3a34f5813ab923603b66e4aa1fc7826b379
SudaKakuto <exce0304@gmail.com>
2019-04-17 11:05:23 +0900

    Merge branch 'master' of https://github.com/japangameaward2019hal01/Globe into Suda
    # Conflicts:
    #	MapMakingStage1/Assets/Scenes/PlanetScene2.unity

---
+++ MapMakingStage1/Assets/User/Suda/scripts/CubeSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSwitch : ObjectController
+{
+    void OnTriggerEnter(Collider hit)
+    {
+        // 接触対象はPlayerタグですか？
+        if (hit.CompareTag("Player"))
+        {
+            OnBotton();
+        }
+    }
+
+}
