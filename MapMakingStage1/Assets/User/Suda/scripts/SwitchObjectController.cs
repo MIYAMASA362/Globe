b7041b5591b56e8260b0795d7d463aeccac90e65
SudaKakuto <exce0304@gmail.com>
2019-04-17 19:43:29 +0900

    発電所などのフラグ起動の起動元と起動先の継承元スクリプト

---
+++ MapMakingStage1/Assets/User/Suda/scripts/SwitchObjectController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchObjectController : MonoBehaviour {
+
+    //起動フラグ
+    public bool button;
+
+	// initialization
+	void Start ()
+    {
+        button = false;
+	}
+
+    //フラグ起動関数
+    public void OnButton()
+    {
+        button = true;
+    }
+}
