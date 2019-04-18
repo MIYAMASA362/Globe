559a7156d3e9179dc099b0f698d49dd2c6655fba
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-04-18 14:57:33 +0900

    PlanetSelectScene用のSceneScripts

---
+++ MapMakingStage1/Assets/User/Ikeda/Scripts/Scene/PlanetSelectScene.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSelectScene : SceneBase
+{
+    private int nPanetNum = 0;
+
+    public override void Start()
+    {
+        base.Start();
+        nPanetNum = 0;
+        MySceneManager.nSelecter_Planet = nPanetNum;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            nPanetNum++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            nPanetNum--;
+        }
+
+        if (nPanetNum <= -1) nPanetNum = MySceneManager.nMaxPlanetNum;
+
+        nPanetNum = nPanetNum % MySceneManager.nMaxPlanetNum;
+
+        if (Input.GetKeyDown(KeyCode.Return)) LoadPlanetScene();
+        MySceneManager.nSelecter_Planet = nPanetNum;
+
+    }
+
+    public void LoadPlanetScene()
+    {
+        MySceneManager.FadeInLoad(MySceneManager.Get_NowPlanet());
+    }
+}
