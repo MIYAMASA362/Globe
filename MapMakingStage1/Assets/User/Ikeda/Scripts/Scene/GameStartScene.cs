f1db0bb69ccaee86c243bea9983019491f28ef33
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-04-23 21:26:22 +0900

    紙芝居を入れる場所を作成

---
+++ MapMakingStage1/Assets/User/Ikeda/Scripts/Scene/GameStartScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStartScene : SceneBase {
+
+    public override void Start()
+    {
+        base.Start();
+        Invoke("Next",2f);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    private void Next()
+    {
+        MySceneManager.FadeInLoad(MySceneManager.GalaxySelect);
+    }
+}
