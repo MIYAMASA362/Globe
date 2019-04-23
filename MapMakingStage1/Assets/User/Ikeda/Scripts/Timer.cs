806bdbd56c846230e500c36166777ed2211ec326
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-04-23 13:08:15 +0900

    Timerクラスの追加

---
+++ MapMakingStage1/Assets/User/Ikeda/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour {
+
+    public float time { get; private set; }
+
+    [SerializeField]
+    private TextMeshProUGUI TimerText;
+
+    [SerializeField]
+    private float MaxTime = 60f* 10f;
+    [SerializeField]
+    private bool bCount = false;
+
+    private void Awake()
+    {
+        time = 0f;
+        bCount = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (bCount)
+        {
+            time += Time.deltaTime;
+        }
+
+        TimerText.text = "Time:"+(int)time;
+	}
+
+    public void StartTimer()
+    {
+        time = 0f;
+        bCount = true;
+    }
+
+    public void StopTimer()
+    {
+        bCount = false;
+    }
+
+    public float GetTime()
+    {
+        return time;
+    }
+}
