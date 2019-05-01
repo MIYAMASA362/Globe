bb44d2298600c8fde4ea911b2afb428d6b149c43
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-05-01 16:32:15 +0900

    各オブジェクトのInspecter用のEditor

---
+++ MapMakingStage1/Assets/Editor/ObjectEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ObjectEditor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+[CustomEditor(typeof(Crystal))]
+public class CrystalEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.HelpBox(
+            "Rot Speedはクリスタルの回転スピード。" +"\n"+
+            "PlanetSceneには各Sceneの[ EventSystem ]もしくは[ PlanetSystem ]の中のPlanetScene.csを入れます。" + "\n"+
+            "PlanetSceneにこのCrystalを登録しないと判定しないので注意!",
+            MessageType.None);
+
+        base.OnInspectorGUI();
+    }
+}
