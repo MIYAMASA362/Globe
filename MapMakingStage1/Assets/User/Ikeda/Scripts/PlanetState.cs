ecd7716a3b4bd1804170e485df41f69f080767b1
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-05-08 11:39:44 +0900

    Planetの情報を持つスクリプト

---
+++ MapMakingStage1/Assets/User/Ikeda/Scripts/PlanetState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetState : MonoBehaviour
+{
+    [Header("StarCrystal State")]
+    [SerializeField, Tooltip("星の宝石の総取得可能数")]
+    public int nMaxStarCrystalNum;
+    [SerializeField, Tooltip("星の宝石の取得数")]
+    public int nGetStarCrystalNum;
+
+    [Space(8)]
+    [Header("Crystal State")]
+    [SerializeField, Tooltip("隠し宝石の総取得可能数")]
+    public int nMaxCrystalNum;
+    [SerializeField, Tooltip("隠し宝石の取得数")]
+    public int nGetCrystalNum;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
