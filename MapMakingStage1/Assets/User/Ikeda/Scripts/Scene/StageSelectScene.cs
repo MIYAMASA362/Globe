5bb90ddfe45cb920d76a37790c4b8a16bdc7c502
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-05-09 21:30:55 +0900

    StageSelectの仕様を変更

--- MapMakingStage1/Assets/User/Ikeda/Scripts/Scene/StageSelectScene.cs
+++ MapMakingStage1/Assets/User/Ikeda/Scripts/Scene/StageSelectScene.cs
@@ -3,14 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Cinemachine;
+using TMPro;
 
-/*
- * どうやって世の中にお金を回すか
- * 労働の対価と幸せの対価
- * 
- * 何に失敗したのか、何をリカバリーしたのか
- * 
- */
 public class StageSelectScene : SceneBase
 {
     //--- Enum ----------------------------------------------------------------
@@ -34,6 +28,46 @@ public class StageSelectScene : SceneBase
 
     //--- Attribute -----------------------------------------------------------
 
+    [Header("UI State")]
+    [SerializeField,Tooltip("銀河の名前")]
+    private TextMeshProUGUI GalaxyNameText;
+    [SerializeField,Tooltip("惑星の名前")]
+    private TextMeshProUGUI PlanetNameText;
+
+    [Space(4)]
+    [SerializeField, Tooltip("銀河の難易度表示")]
+    private TextMeshProUGUI GalaxyLevelText;
+    [SerializeField, Tooltip("惑星の難易度表示")]
+    private TextMeshProUGUI PlanetLevelText;
+
+    [Space(4)]
+    [Header("ItemUI")]
+    [SerializeField, Tooltip("未取得アイテムのUI表示")]
+    private GameObject ItemUI;
+    [SerializeField,Tooltip("星の宝石数を表示するテキスト")]
+    private TextMeshProUGUI ItemUI_Text_StarCrystal;
+    [SerializeField, Tooltip("隠し宝石数を表示するテキスト")]
+    private TextMeshProUGUI ItemUI_Text_Crystal;
+    [Space(4)]
+    [SerializeField, Tooltip("銀河選択時の説明テキスト")]
+    private GameObject ItemUI_Obj_GalaxyText;
+    [SerializeField, Tooltip("惑星選択時の説明テキスト")]
+    private GameObject ItemUI_Obj_PlanetText;
+
+    [Space(4)]
+    [Header("LockUI")]
+    [SerializeField, Tooltip("未解除エリアのUI表示")]
+    private GameObject LockUI;
+    [SerializeField]
+    private TextMeshProUGUI LockUI_Text;
+
+    [Space(4)]
+    [SerializeField, Tooltip("エリア選択時のUIオブジェクト")]
+    private GameObject GalaxySelectUI;
+    [SerializeField, Tooltip("ステージ選択時のUIオブジェクト")]
+    private GameObject PlanetSelectUI;
+
+    [Space(8)]    
     [Header("StarCrystal State")]
     [SerializeField, Tooltip("星の宝石の総取得可能数")]
     private int nMaxStarCrystalNum;
@@ -47,7 +81,7 @@ public class StageSelectScene : SceneBase
     [SerializeField,Tooltip("隠し宝石の取得数")]
     private int nGetCrystalNum;
 
-    [Space(8)]
+    [Space(4)]
     [Header("Select State")]
     [SerializeField, Tooltip("エリア選択時のカメラ")]
     private GameObject galaxyCamera;
@@ -69,14 +103,17 @@ public class StageSelectScene : SceneBase
     //--- Internal State ------------------------
     [SerializeField] private STATE state = STATE.GALAXYSELECT;  //状態遷移
     [SerializeField] private bool IsInput = false;              //入力可能か
-    [SerializeField] private float selecter = 0f;               //入力値
-    [SerializeField] private float TargetAngle = 0f;            //回転値
-    [SerializeField] private GameObject TargetRotObj = null;    //回転させるオブジェクト
+    [SerializeField] private float IsInput_PauseTime = 0f;      //IsInputをfalse状態にする時間
+    private bool IsInputLeft = false;           //左入力
+    private bool IsInputRight = false;          //右入力
+    private float TargetAngle = 0f;             //回転値
+    private GameObject TargetRotObj = null;     //回転させるオブジェクト
 
     //--- const ---------------------------------
-    private const float GalaxyRot = 90f;    //Galaxyの回転量
-    private const float PlanetRot = 72f;    //Planetの回転量
-    private const float RotSpeed = 8f;      //回転速度
+    private const float GALAXY_ROTARION_ANGLE = 90f;    //Galaxyの回転量
+    private const float PLANET_ROTATION_ANGLE = 72f;    //Planetの回転量
+    private const float ROTATION_SPEED = 8f;      //回転速度
+    private const float ISINPUT_POUSETIME = 0.5f;
 
     //--- MonoBehaviour -------------------------------------------------------
 
@@ -85,13 +122,13 @@ public class StageSelectScene : SceneBase
     {
         base.Start();
 
+        //---　初期化(初期状態に必要なパラメータなどにアクセスして調整する) ---
+
         //星の宝石の設定
         nMaxStarCrystalNum = 0;
-        nGetStarCrystalNum = 0;
 
         //隠し宝石の設定
         nMaxCrystalNum = 0;
-        nGetCrystalNum = 0;
 
         //GalaxyStateを設定し、情報取得
         foreach (Galaxy galaxy in Galaxies)
@@ -121,14 +158,30 @@ public class StageSelectScene : SceneBase
         nMaxGalaxyNum = MySceneManager.Instance.Galaxies.Count;
         nMaxPlanetNum = MySceneManager.Instance.Galaxies[nPlanetSelectNum].Path_Planets.Count;
 
+        //UIを初期化
+        GalaxySelectUI.SetActive(false);
+        PlanetSelectUI.SetActive(false);
+
         //GalaxyCameraを設定
-        if (state == STATE.GALAXYSELECT) galaxyCamera.SetActive(true);
+        if (state == STATE.GALAXYSELECT)
+        {
+            galaxyCamera.SetActive(true);
+            GalaxySelectUI.SetActive(true);
+        }
+        
+        //--- 初期状態設定 ----------------------------
 
         //保存データ更新
         SelectDataUpdate();
 
         //回転させる対象を設定
         TargetRotObj = GalaxysHolder;
+
+        //テキストの設定
+        GalaxyNameText.text = Galaxies[nGalaxySelectNum].galaxyState.GalaxyName;
+
+        //Lockの切り替え
+        Change_Lock_UnLock(true);
     }
 	
 	// Update is called once per frame
@@ -136,20 +189,28 @@ public class StageSelectScene : SceneBase
     {
         base.Update();
 
+        IsInputLeft = Input.GetButtonDown(InputManager.Left_AxisRotation);
+        IsInputRight = Input.GetButtonDown(InputManager.Right_AxisRotation);
+
         //入力取得
-        selecter = Input.GetAxis(InputManager.X_Selecter);
-        if (selecter == 0f) IsInput = true;
+        IsInput_PauseTime -= Time.deltaTime;
+        if (IsInput_PauseTime < 0f) IsInput_PauseTime = 0f;
+        if (IsInput_PauseTime == 0f) IsInput = true;
 
         //state分岐
         switch (state)
         {
             case STATE.GALAXYSELECT:
+                //更新処理
                 GalaxySelect_Update();
+                //カメラのアクティブ確認
                 if(galaxyCamera.activeSelf)
                     galaxyCamera.SetActive(true);
                 break;
             case STATE.PLANETSELECT:
+                //更新処理
                 PlanetSelect_Update();
+                //カメラのアクティブ確認
                 if (Galaxies[nGalaxySelectNum].planetCamera.activeSelf)
                     Galaxies[nGalaxySelectNum].planetCamera.SetActive(true);
                 break;
@@ -159,7 +220,12 @@ public class StageSelectScene : SceneBase
 
         Quaternion q1 = TargetRotObj.transform.rotation;
         Quaternion q2 = Quaternion.Euler(0f, TargetAngle, 0f);
-        TargetRotObj.transform.rotation = Quaternion.Lerp(q1, q2, Time.deltaTime * RotSpeed);
+        TargetRotObj.transform.rotation = Quaternion.Lerp(q1, q2, Time.deltaTime * ROTATION_SPEED);
+    }
+
+    private void LateUpdate()
+    {
+        
     }
 
     private void OnDrawGizmos()
@@ -177,26 +243,24 @@ public class StageSelectScene : SceneBase
     //--- エリア選択 ----------------------------
     public void GalaxySelect_Update()
     {
+        //選択更新
+        if (!IsInput) return;
+
         //前フレーム変更格納
         int nOld = nGalaxySelectNum;
-
-        //選択更新
-        if (IsInput)
+        if (IsInputLeft)
         {
-            if(selecter >= 0.5f)
-            {
-                nGalaxySelectNum++;
-                IsInput = false;
-            }
-
-            if(selecter <= -0.5f)
-            {
-                nGalaxySelectNum--;
-                IsInput = false;
-            }
+            nGalaxySelectNum++;
+            IsInput = false;
         }
 
-        //更新されている
+        if(IsInputRight)
+        {
+            nGalaxySelectNum--;
+            IsInput = false;
+        }
+
+        //選択が更新されている
         if (nOld != nGalaxySelectNum)
         {
             //マイナス値ならば
@@ -207,18 +271,35 @@ public class StageSelectScene : SceneBase
             SelectDataUpdate();
 
             //回転対象を設定
-            Set_RotTargetState(GalaxysHolder,GalaxyRot * nGalaxySelectNum);
+            Set_RotTargetState(GalaxysHolder,GALAXY_ROTARION_ANGLE * nGalaxySelectNum);
+
+            //UI更新
+            Change_SelectUI(state);
+            Change_Lock_UnLock(true);
+
+            //入力Pause
+            IsInput_PauseTime = ISINPUT_POUSETIME;
         }
 
         //決定キーを確認
-        if (Input.GetButtonDown(InputManager.Submit))
+        if (Galaxy_Submit())
         {
+            nPlanetSelectNum = 0;
+
             //カメラ切り替え
             Galaxies[nGalaxySelectNum].planetCamera.SetActive(true);
             //最大数を更新
             nMaxPlanetNum = MySceneManager.Instance.Galaxies[nGalaxySelectNum].Path_Planets.Count;
             //遷移を更新
             state = STATE.PLANETSELECT;
+
+            //回転対象を設定
+            Set_RotTargetState(Galaxies[nGalaxySelectNum].galaxyState.PlanetParent, PLANET_ROTATION_ANGLE * nPlanetSelectNum);
+
+            //UI変更
+            Change_SelectUI(state);
+            Change_Lock_UnLock(false);
+            Change_ItemUI(true);
         }
 
         if(Input.GetButtonDown(InputManager.Cancel))
@@ -228,23 +309,21 @@ public class StageSelectScene : SceneBase
     //--- 惑星選択 ------------------------------
     public void PlanetSelect_Update()
     {
+        //選択更新
+        if (!IsInput) return;
         //前フレーム変更格納
         int nOld = nPlanetSelectNum;
-
-        //選択更新
-        if (IsInput)
+        
+        if (IsInputLeft)
         {
-            if (selecter >= 0.5f)
-            {
-                nPlanetSelectNum++;
-                IsInput = false;
-            }
+            nPlanetSelectNum++;
+            IsInput = false;
+        }
 
-            if (selecter <= -0.5f)
-            {
-                nPlanetSelectNum--;
-                IsInput = false;
-            }
+        if (IsInputRight)
+        {
+            nPlanetSelectNum--;
+            IsInput = false;
         }
 
         //更新されている
@@ -258,7 +337,14 @@ public class StageSelectScene : SceneBase
             SelectDataUpdate();
 
             //回転対象を設定
-            Set_RotTargetState(Galaxies[nGalaxySelectNum].galaxyState.PlanetParent, PlanetRot * nPlanetSelectNum);
+            Set_RotTargetState(Galaxies[nGalaxySelectNum].galaxyState.PlanetParent, PLANET_ROTATION_ANGLE * nPlanetSelectNum);
+
+            //UI更新
+            Change_SelectUI(state);
+            Change_ItemUI(true);
+
+            //入力Pause
+            IsInput_PauseTime = ISINPUT_POUSETIME;
         }
 
         //決定キーを確認
@@ -271,14 +357,103 @@ public class StageSelectScene : SceneBase
         {
             state = STATE.GALAXYSELECT;
             Galaxies[nGalaxySelectNum].planetCamera.SetActive(false);
+            //UI変更
+            Change_SelectUI(state);
+            Change_Lock_UnLock(true);
         }
     }
 
-    //--- 回転の対象設定 --------------------------
+    //--- 回転の対象設定 ------------------------
     void Set_RotTargetState(GameObject target,float Angle)
     {
         TargetRotObj = target;
         TargetAngle = Angle;
+    }
+
+    //--- 選択UIの更新　-------------------------
+    void Change_SelectUI(STATE state)
+    {
+        switch (state)
+        {
+            case STATE.GALAXYSELECT:
+                //UIConfigなどを更新
+                GalaxySelectUI.SetActive(true);
+                PlanetSelectUI.SetActive(false);
+                break;
+            case STATE.PLANETSELECT:
+                //UIConfigなどを更新
+                GalaxySelectUI.SetActive(false);
+                PlanetSelectUI.SetActive(true);
+                break;
+            default:
+                break;
+        }
+
+        //銀河名更新
+        GalaxyNameText.text = Galaxies[nGalaxySelectNum].galaxyState.GalaxyName;
+        //惑星名更新
+        PlanetNameText.text = Galaxies[nGalaxySelectNum].galaxyState.Planets[nPlanetSelectNum].PlanetName;
+
+        //銀河LEVEL名変更
+        GalaxyLevelText.text = "エリア " + (nGalaxySelectNum + 1);
+        //惑星LEVEL名変更
+        PlanetLevelText.text = (nGalaxySelectNum + 1)+ "-" + (nPlanetSelectNum+1);
+    }
+
+    //LockとUnLockを変更　IsActiveで表示の有効化、無効化
+    void Change_Lock_UnLock(bool IsActive)
+    {
+        //Activeであるならば
+        if(LockUI.activeSelf) LockUI.SetActive(false);
+        if(ItemUI.activeSelf) ItemUI.SetActive(false);
+
+        if (!IsActive) return;
+
+        //Lockされた
+        bool IsLock = Galaxies[nGalaxySelectNum].galaxyState.CheckLock(nGetCrystalNum);
+        //銀河の情報
+        GalaxyState galaxyState = Galaxies[nGalaxySelectNum].galaxyState;
+        if (IsLock)
+        {
+            //ロックされているのか表示
+            LockUI.SetActive(true);
+            LockUI_Text.text = "あと" + galaxyState.Crtstal_Diffrence(nGetCrystalNum)+"こ";
+        }
+        else
+            Change_ItemUI(IsActive);
+    }
+
+    //ItemUIの表示 IsActiveで表示の有効、無効変換
+    void Change_ItemUI(bool IsActive)
+    {
+        //Activeであるならば
+        if(ItemUI.activeSelf) ItemUI.SetActive(false);
+        if(ItemUI_Obj_GalaxyText.activeSelf) ItemUI_Obj_GalaxyText.SetActive(false);
+        if(ItemUI_Obj_PlanetText.activeSelf) ItemUI_Obj_PlanetText.SetActive(false);
+
+        if (!IsActive) return;
+
+        GalaxyState galaxyState = Galaxies[nGalaxySelectNum].galaxyState;
+        switch (state)
+        {
+            case STATE.GALAXYSELECT:
+                
+                //ステージの内容表示
+                ItemUI.SetActive(true);
+                ItemUI_Obj_GalaxyText.SetActive(true);
+                ItemUI_Text_Crystal.text = "あと" + galaxyState.Crystal_RemainingNum() + "こ";
+                ItemUI_Text_StarCrystal.text = "あと" + galaxyState.StarCrystal_ReaminingNum() + "こ";
+                break;
+            case STATE.PLANETSELECT:
+                PlanetState planetState = galaxyState.Planets[nPlanetSelectNum];
+                ItemUI.SetActive(true);
+                ItemUI_Obj_PlanetText.SetActive(true);
+                ItemUI_Text_Crystal.text = "あと" + planetState.StarCrystal_ReaminingNum() + "こ";
+                ItemUI_Text_StarCrystal.text = "あと" + planetState.StarCrystal_ReaminingNum() + "こ";
+                break;
+            default:
+                break;
+        }
     }
 
     //--- 入力系 ------------------------------------------
@@ -291,7 +466,8 @@ public class StageSelectScene : SceneBase
         //決定キーが押された
         if (!Input.GetButtonDown(InputManager.Submit)) return false;
         //銀河のロックが解除されるか
-        if (!Galaxies[nGalaxySelectNum].galaxyState.CheckLock(nGetCrystalNum)) return false;
+        if (Galaxies[nGalaxySelectNum].galaxyState.CheckLock(nGetCrystalNum)) return false;
+        IsInput_PauseTime = ISINPUT_POUSETIME;
         return true;
     }
 
@@ -300,6 +476,8 @@ public class StageSelectScene : SceneBase
     {
         //決定キーが押された
         if (!Input.GetButtonDown(InputManager.Submit)) return false;
+        IsInput_PauseTime = ISINPUT_POUSETIME;
+        
         return true;
     }
 
@@ -322,5 +500,4 @@ public class StageSelectScene : SceneBase
     {
         obj.transform.Find("Canvas").gameObject.SetActive(enable);
     }
-
 }
