e350e7f1c94eef3a9918ce1952187a734624aa49
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-05-20 21:20:22 +0900

    ResultEndのアニメーションで呼ぶEvent

---
+++ MapMakingStage1/Assets/User/Ikeda/Scripts/PlanetScene/ResultEndEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultEndEvent : MonoBehaviour
+{
+
+    [SerializeField] private PlanetScene planetScene;
+    private PlanetResult planetResult;
+
+	// Use this for initialization
+	void Start ()
+    {
+        planetResult = planetScene.GetComponent<PlanetResult>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void NextScene()
+    {
+        planetScene.NextScene();
+    }
+
+    public void PrintResult()
+    {
+        planetResult.Print();
+    }
+}
