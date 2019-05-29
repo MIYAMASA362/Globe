1e433399f4d770c5c88e2e73ff55cd096683bff7
THS70915\yanase.taiho <taiho0423@gmail.com>
2019-05-30 02:39:58 +0900

    デバイスの動き調整

--- MapMakingStage1/Assets/Features/Scritps/AtmosphereManager.cs
+++ MapMakingStage1/Assets/Features/Scritps/AtmosphereManager.cs
@@ -10,6 +10,8 @@ public class AtmosphereManager : Singleton<AtmosphereManager>
     public float cloudsScaleSmooth = 5f;
     public float cloudsTargetScale = 5f;
 
+    private Vector3 targetScale;
+
     private Vector3 initScale = Vector3.zero;
 
     void Start()
@@ -21,12 +23,14 @@ public class AtmosphereManager : Singleton<AtmosphereManager>
     {
         if (CameraManager.Instance.characterCamera.gameObject.activeInHierarchy)
         {
-            clouds.transform.localScale = Vector3.Lerp(clouds.transform.localScale, new Vector3(cloudsTargetScale, cloudsTargetScale, cloudsTargetScale), Time.deltaTime * cloudsScaleSmooth);
+            targetScale = new Vector3(cloudsTargetScale, cloudsTargetScale, cloudsTargetScale);
         }
         else
         {
-            clouds.transform.localScale = Vector3.Lerp(clouds.transform.localScale, initScale, Time.deltaTime * cloudsScaleSmooth);
+            targetScale = initScale;
         }
+
+        clouds.transform.localScale = Vector3.Lerp(clouds.transform.localScale, initScale, Time.deltaTime * cloudsScaleSmooth);
     }
 
     private void LateUpdate()
