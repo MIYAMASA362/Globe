b882ccb2a7e156cb9122dd85b4a2da2e4d8a314a
THS70915\yanase.taiho <taiho0423@gmail.com>
2019-05-31 04:30:16 +0900

    クリア時軸を消す

---
+++ MapMakingStage1/Assets/MeshClear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshClear : MonoBehaviour {
+
+    MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+        meshRenderer = GetComponent<MeshRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (PlanetScene.Instance.state == PlanetScene.STATE.RESULT)
+        {
+            meshRenderer.enabled = false;
+        }
+    }
+}
